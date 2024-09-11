1669e56fbd50f3d396d7354d720866810b83dc5c
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-09-10 21:41:22 -0700

    created component ImageGallery.vue

---
+++ 05-Vue_Expanded/assignment/assignment-5/src/components/About.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import { ImageGallery } from "/ImageGallery.vue";
+</script>
+
+<template>
+  <main class="main">
+      <h1 class="display-3 text-lowercase bg-warning-subtle px-3 pb-2">About the theater</h1>
+      <div class="p-3">
+          <p class="lead">The Seattle Cinema is the longest-running independent cinema in the city, and has become a landmark of the film community.</p>
+          <p>Opened in 1970 by Randy Findley in a converted dentist’s office, the cinema became the city’s first arthouse, showcasing foreign and revival films. The cinema was such a success it led to Randy’s opening the Seven Gables Cinema and eventually starting the local Seven Gables Theatres chain, which inevitably got swallowed by the national Landmark Cinema Corporation. The Seattle Cinema was never part of the Seven Gables chain; it has remained a popular independent venue.</p>
+          <p>A nonprofit film arts organization, the Northwest Film Forum, saved the theater from closure in 1997, remodeled it, and revitalized interest in the venerable institution. Since 2004, a strong group of dedicated staff and volunteers focus attention and energy on continuing the tradition of this treasured cinema. Over the years, literally hundreds of cinema classics, rarities and truly independent cinema from around the world have been presented in the charming confines of The Seattle Cinema.</p>
+      </div>
+  </main>
+  <!--Image Gallery imported from ImageGallery.vue-->
+  <section>
+    <ImageGallery />   
+  </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ 05-Vue_Expanded/assignment/assignment-5/src/components/ImageGallery.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+
+const photos = ref([
+      { img: '/src/assets/front-door.jpg', title: 'exterior photo of the front door entrance to Seattle Cinema' },
+      { img: '/src/assets/interior2.jpg', title: 'interior photo of the theater showing the seats from the front of the auditorium' },
+      { img: '/src/assets/interior.jpg', title: 'interior photo of the theater showing the movie screen from the back of the auditorium' },
+      { img: '/src/assets/concessions.png', title: 'interior photo of the concessions stand featuring glowing neon lights' },
+    ]
+)
+
+</script>
+
+<template>
+  <!--Image Gallery. All the images have responsive sizes and the image layout is responsive. Images are also generated dynamically using variables and a "v-for" loop-->
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-xl-6 col-md-6 col-xs-12" v-for="photo in photos" :key="photo">
+        <figure>
+          <img :src="photo.img" class="img-fluid border border-black" width="640" :alt="photo.title">
+        </figure>
+      </div>            
+    </div>  
+  </div>    
+</template>
+
+<style scoped>
+
+</style>
