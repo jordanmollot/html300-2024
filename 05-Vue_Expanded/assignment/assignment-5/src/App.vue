01aee8995c84836cd263b0c480dfb5e94cc29a17
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-09-15 11:06:29 -0700

    created HeaderComponent.vue for the header component and used a slot for the header

--- 05-Vue_Expanded/assignment/assignment-5/src/App.vue
+++ 05-Vue_Expanded/assignment/assignment-5/src/App.vue
@@ -4,6 +4,7 @@ import Home from "./components/Home.vue";
 import About from "./components/About.vue";
 import Showtimes from "./components/Showtimes.vue";
 import Rental from "./components/Rental.vue";
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 const routes = {
   "/": Home,
@@ -26,36 +27,39 @@ const currentView = computed(() => {
 </script>
 
 <template>
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning p-4">
-      <a class="navbar-brand" href="index.html">The Seattle Cinema</a>
-      <button
-          class="navbar-toggler"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-      >
-      <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-              <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#/">Home</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#/about">About</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#/showtimes">Showtimes</a>
-              </li>                        
-              <li class="nav-item">
-                  <a class="nav-link" href="#/rental">Rental</a>
-              </li>
-          </ul>
-      </div>
-  </nav>
+  <HeaderComponent>
+    <template v-slot:header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-warning p-4">
+        <a class="navbar-brand" href="index.html">The Seattle Cinema</a>
+        <button
+            class="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#/showtimes">Showtimes</a>
+                </li>                        
+                <li class="nav-item">
+                    <a class="nav-link" href="#/rental">Rental</a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    </template>  
+  </HeaderComponent>
 
   <component :is="currentView" />
   <footer class="p-4 bg-warning">The Seattle Cinema, copyright 2024</footer>
---
+++ 05-Vue_Expanded/assignment/assignment-5/src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+</script>
+
+<template>
+    <header>
+        <slot name="header"></slot>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
